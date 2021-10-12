873d4ba43cea2936efa92b6d44623545e5f5e034
webster <wbian618@gmail.com>
2021-10-12 17:33:55 -0400

    commit tree b8d8e05db8f39555308a284f8a2cf37c7f891fc2

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feetValue) {
+  if (feetValue <= 400) {
+  return 'This one is on me!';
+  }
+  else if (feetValue < 2500 ) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feetValue >= 2500 ) {
+    return 'No can do.';
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
+    return city == 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
+  
+  
+  
+
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous': return 'Thank you so much.'
+    break;
+    case 'not as generous': return 'Thank you.'
+    break;
+    default: return 'Bye.'
+  }
   // Write your code here!
+  
 }
